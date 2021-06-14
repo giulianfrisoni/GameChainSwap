eb94995b53f6ea7154acf4b0b02a5e23f7578fc5
giulianfrisoni <giulianfrisoni@gmail.com>
2021-06-13 22:32:29 -0600

    Addign addtional files src and html js components

---
+++ migrations/2_initial_migration.js
@@ -0,0 +1,13 @@
+const GameChainSwap = artifacts.require("GameChainSwap");
+const GameChainToken = artifacts.require("GameChainToken");
+module.exports = async function(deployer) {
+// Deploy GameSwap SmartContract - Engine of Swap 
+  await deployer.deploy(GameChainSwap);
+  const gameChainSwap = await GameChainSwap.deployed()
+// Deploy GameSwap Token  
+  await deployer.deploy(GameChainToken);
+  const gameChainToken = await GameChainToken.deployed()
+
+// All tokens to Swap ( 1 Million ) 
+await gameChainToken.transfer(gameChainSwap.address,'1000000000000000000000000')
+};
